7a36f89b36d5ca5143057f2ebffd20190adedc66
purwoajifa <purwoaifahmia@gmail.com>
2018-10-31 21:12:58 +0700

    alhamdulillah

--- src/main/java/com/apap/tutorial7/rest/Setting.java
+++ src/main/java/com/apap/tutorial7/rest/Setting.java
@@ -2,5 +2,5 @@ package com.apap.tutorial7.rest;
 
 public class Setting {
 	final public static String dealerUrl = "https://ac313aa2-a627-4da6-bff9-cc26dfafa108.mock.pstmn.io";
-
+	final public static String modelUrl = "https://www.carqueryapi.com/api/0.3";
 }
